1f406c7c8a76098449e302559c0362d40804c5b1
jaofe <104651266+jaofe@users.noreply.github.com>
2023-05-04 11:00:20 -0300

    Add files via upload

---
+++ Spring/biblio/custonExceptions/InvalidCommandException.java
@@ -0,0 +1,7 @@
+package biblioteca.biblio.custonExceptions;
+
+public class InvalidCommandException extends Exception{
+    public InvalidCommandException(String message) {
+        super(message);
+    }
+}
